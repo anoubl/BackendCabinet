a5f69bbeabb7797079e271055c8bf66426ac1189
Ahmed Amine <98238861+anoubl@users.noreply.github.com>
2023-12-01 20:30:18 +0100

    Ajoutez des fichiers projet.

---
+++ DataDB/CabinetContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendCabinet.DataDB;
+
+public partial class CabinetContext : DbContext
+{
+    public CabinetContext()
+    {
+    }
+
+    public CabinetContext(DbContextOptions<CabinetContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=ahmed;Initial Catalog=Cabinet;Integrated Security=True;Encrypt=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => new { e.Id, e.Email }).HasName("PK_users_id_email");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Nom)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("nom");
+            entity.Property(e => e.Password)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Prenom)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("prenom");
+            entity.Property(e => e.Telephone)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("telephone");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DataDB/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackendCabinet.DataDB;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string? Prenom { get; set; }
+
+    public string? Nom { get; set; }
+
+    public string Email { get; set; } = null!;
+
+    public string? Password { get; set; }
+
+    public string? Telephone { get; set; }
+}
